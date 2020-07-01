1fdacfd47fbe70382796ef1d32e1abfaa748e2f4
Andrew Cowley <andrew@andrewcowley.net>
2020-07-01 22:38:27 +1000

    ss

--- src/http/get-api/index.js
+++ src/http/get-api/index.js
@@ -4,7 +4,7 @@ exports.handler = async function http() {
   let table = 'water_meter'
   let key = `m1` 
   let pulses = 450
-  await data.set({table, key, greeting})
+  await data.set({table, key, pulses})
   // Increment
   await data.incr({table, key, prop: 'pulses'})
 }
