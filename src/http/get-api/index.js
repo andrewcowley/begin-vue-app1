2f8a4579d6156df31c88aaec8595fd60598b9cf5
Andrew Cowley <andrew@andrewcowley.net>
2020-07-01 22:36:20 +1000

    setting table

--- src/http/get-api/index.js
+++ src/http/get-api/index.js
@@ -3,6 +3,8 @@ let data = require('@begin/data')
 exports.handler = async function http() {
   let table = 'water_meter'
   let key = `m1` 
+  let pulses = 450
+  await data.set({table, key, greeting})
   // Increment
   await data.incr({table, key, prop: 'pulses'})
 }
