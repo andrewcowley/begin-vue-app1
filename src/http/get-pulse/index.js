1c3ab72546873fa7f80820926311f47abc5a1162
Andrew Cowley <andrew@andrewcowley.net>
2020-07-10 00:19:41 +1000

    asdahhss s

--- src/http/get-pulse/index.js
+++ src/http/get-pulse/index.js
@@ -17,14 +17,16 @@
 // }
 exports.handler = async function http (req) {
   console.log('Begin API called')
-  const doc = 'Hello from your Begin API doc!'
+  let table = await data.get({
+    table: 'inc'
+  })
   return {
     headers: {
       'content-type': 'application/json; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
     body: JSON.stringify({
-      message: doc
+      message: table
     })
   }
 }
