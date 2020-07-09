30ae8dbe24fbeaa3de8be75a36258376eb31ea0c
Andrew Cowley <andrew@andrewcowley.net>
2020-07-09 23:53:51 +1000

    asdahhs

---
+++ src/http/get-pulse/index.js
@@ -0,0 +1,19 @@
+let data = require('@begin/data')
+
+exports.handler = async function post (req) {
+ 
+  let table = 'inc';
+  let key = 'pulses';
+
+  let doc = await data.get({table, key})
+  return {
+    statusCode: 201,
+    headers: {
+      'content-type': 'application/json; charset=utf8',
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+    },
+    body: JSON.stringify({
+      doc
+    })
+  }
+}
