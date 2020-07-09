d97a34889cd530ab30b3fb4640c68899e2c3933b
Andrew Cowley <andrew@andrewcowley.net>
2020-07-10 00:13:22 +1000

    asdahhss s

--- src/http/get-pulse/index.js
+++ src/http/get-pulse/index.js
@@ -1,17 +1,29 @@
 let data = require('@begin/data')
 
+// exports.handler = async function http (req) {
+//   let table = 'inc';
+//   let key = 'pulses';
+//   let doc = await data.get({table, key})
+//   return {
+//     statusCode: 201,
+//     headers: {
+//       'content-type': 'application/json; charset=utf8',
+//       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+//     },
+//     body: JSON.stringify({
+//       doc
+//     })
+//   }
+// }
 exports.handler = async function http (req) {
-  let table = 'inc';
-  let key = 'pulses';
-  let doc = await data.get({table, key})
+  console.log('Begin API called')
   return {
-    statusCode: 201,
     headers: {
       'content-type': 'application/json; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
     body: JSON.stringify({
-      doc
+      message: 'Hello from your Begin API!'
     })
   }
 }
