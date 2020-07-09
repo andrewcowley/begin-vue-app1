e1cb699b76509e50d192f2f8eda2075df4954d07
Andrew Cowley <andrew@andrewcowley.net>
2020-07-09 23:50:35 +1000

    asdahh

--- src/http/post-pulse/index.js
+++ src/http/post-pulse/index.js
@@ -5,7 +5,7 @@ exports.handler = async function post (req) {
   let table = 'inc';
   let key = 'pulses';
 
-  await data.incr({table, key, prop: 'pulses'})
+  await data.incr({table, key})
   return {
     statusCode: 200
   }
